e96a6ed2601c8e90db79a7af248e3d2ec6534257
MarcoPerez29 <marckedy29@gmail.com>
2025-03-15 08:42:51 -0600

    Se agrego componentDidMount en el componente de App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import Header from "./components/Header";
 import Song from "./components/Song";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <section className="song-list">
-        <Song title ='Leave The Door Open' artist='Bruno Mars' duration='4:20' />
-        <Song title ='Lose Control' artist='Teddy Swims' duration='3:30' />
-        <Song title ='Crazy' artist='Gnarls Barkle' duration='3:02' />
-      </section>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    console.log("La aplicación se ha cargado correctamente");
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Header/>
+          <section className="song-list">
+            <Song title ='Leave The Door Open' artist='Bruno Mars' duration='4:20' />
+            <Song title ='Lose Control' artist='Teddy Swims' duration='3:30' />
+            <Song title ='Crazy' artist='Gnarls Barkle' duration='3:02' />
+          </section>
+        </div>
+      );
+  }
+  
 }
 
 export default App;
